5d2cf3f846cf0dd9e4d4f458cc389eb75ba72177
MLO034 <jonalgaming@gmail.com>
2023-07-23 17:12:02 +0200

    Bug fix test/pushing

--- helper/file_helper.py
+++ helper/file_helper.py
@@ -17,9 +17,8 @@ def create_domain_super_list(filename, domains, message):
     print(message)
     file_path = os.path.join("Output", filename)
     with open(file_path,"w") as f:
-        merged = list(chain(*domains)) # merges all array togheter
-        merged_no_duplicates = list(dict.fromkeys(merged)) # removes duplicates
-        print("I recived all of these domains: ", merged_no_duplicates , "of type: ",type(domains))
+        merged_no_duplicates = list(set(domains)) # removes duplicates
+        print("I received all of these domains: ", merged_no_duplicates , "of type: ",type(domains))
         for domain in merged_no_duplicates:
             print("writing,",domain)
             f.write(domain+ "\n")
